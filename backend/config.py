11a0fc56e995aad2b870bcfad895a1bf81436c59
DivYonko <divyamvns21@gmail.com>
2026-04-09 20:38:12 +0530

    first commit

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+import os
+
+VIDEO_ID   = os.getenv("VIDEO_ID", "J3qcYJAhCMY")
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_DB   = int(os.getenv("REDIS_DB", 0))
